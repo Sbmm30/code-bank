bd95f02b244640af3cd9a0f20358885c04d5619e
Sebastian Mori <mori.sebastian@outlook.com>
2023-03-13 00:02:14 -0500

    se modifico el diseño de login, agregando un card al formulario

--- src/templates/views/login.php
+++ src/templates/views/login.php
@@ -24,7 +24,7 @@
       <div class="row g-0">
         <div class="bgImage col-md-4 col-lg-6 d-none d-md-flex">
           <div>
-            <img src="../../assets/images/payment-successful.png" alt="card" class="img-fluid">
+            <img src="../../assets/images/payment-successful.png" alt="card-img" class="img-fluid">
           </div>
         </div>
         <div class="col-md-8 col-lg-6">
@@ -40,89 +40,95 @@
             </div>
           </div>
           <div class="login d-flex align-items-center">
-            <div class="container">
+            <div class="container my-3">
               <div class="row">
                 <div class="col-md-9 col-lg-8 mx-auto">
-                  <h3 class="login-heading mb-4">Banca por internet</h3>
+                  <div class="shadow-lg mb-5 bg-body-tertiary rounded-5">
+                    <div class="card text-start rounded-5">
+                      <div class="card-body m-4">
+                        <h3 class="login-heading mb-4">Banca por internet</h3>
 
-                  <!-- Sign In Form -->
-                  <form>
+                        <!-- Sign In Form -->
+                        <form>
 
-                    <div class="form-floating mb-3">
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="option" id="txtPerson" value=""
-                          onchange="typeUser(this)">
-                        <label class="form-check-label" for="txtPerson">Persona</label>
-                      </div>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="option" id="txtCompany" value="4912"
-                          onchange="typeUser(this)">
-                        <label class="form-check-label" for="txtCompany">Empresa</label>
+                          <div class="form-floating mb-3">
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="option" id="txtPerson" value=""
+                                onchange="typeUser(this)">
+                              <label class="form-check-label" for="txtPerson">Persona</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="option" id="txtCompany" value="4912"
+                                onchange="typeUser(this)">
+                              <label class="form-check-label" for="txtCompany">Empresa</label>
+                            </div>
+                          </div>
+
+                          <div class="form-floating input-group mb-3 gap-2">
+                            <select class="btn btn-outline-secondary rounded-3">
+                              <option value="">DNI</option>
+                              <option value="">CE</option>
+                              <option value="">PAS</option>
+                            </select>
+                            <div class="form-floating">
+                              <input type="text" class="form-control rounded-3" id="txtNrodocument"
+                                placeholder="Nro-document" maxlength="8" required autocomplete="off">
+                              <label for="txtNrodocument">Nro de documento</label>
+                            </div>
+                          </div>
+
+                          <div class="form-floating mb-3">
+                            <input type="text" class="form-control rounded-3" id="txtNum-card" placeholder="No-card"
+                              required autocomplete="off">
+                            <label for="txtNum-card">Número de tarjeta</label>
+                          </div>
+
+                          <div>
+                            <div class="form-check mb-3">
+                              <input class="form-check-input" type="checkbox" value="" id="chkCardName">
+                              <label class="form-check-label" for="chkCardName">
+                                Recordar datos
+                              </label>
+                            </div>
+                            <div class="form-floating mb-3 d-none" id="inputToShow">
+                              <input type="text" class="form-control" id="txtNum-card" placeholder="No-card"
+                                autocomplete="off">
+                              <label for="txtNum-card">Nombre de tarjeta</label>
+                            </div>
+                          </div>
+
+                          <div class="form-floating mb-3">
+                            <input type="password" class="form-control rounded-3" id="floatingInput"
+                              placeholder="Password" maxlength="6" required autocomplete="off">
+                            <label for="floatingInput">Clave de internet de 6 dígitos</label>
+                            <div class="row">
+                              <div class="col-6">
+                                <a class="small" href="#">No tengo clave</a>
+                              </div>
+                              <div class="col-6 text-end">
+                                <a class="small" href="#">Olvide mi clave</a>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="form-floating mb-3">
+                            <input type="text" class="form-control rounded-3" id="floatingPassword" placeholder="Code"
+                              required autocomplete="off">
+                            <label for="floatingPassword">Código</label>
+                            <div class="text-start">
+                              <a class="small" href="#">Cambiar código</a>
+                            </div>
+                          </div>
+
+                          <div class="d-grid">
+                            <button class="btn btn-lg btn-success btn-login text-uppercase fw-bold mb-2" type="submit"
+                              onclick="setDNI()">Continuar</button>
+                          </div>
+
+                        </form>
                       </div>
                     </div>
-
-                    <div class="form-floating input-group mb-3">
-                      <select class="btn btn-outline-secondary">
-                        <option value="">DNI</option>
-                        <option value="">CE</option>
-                        <option value="">PAS</option>
-                      </select>
-                      <div class="form-floating">
-                        <input type="text" class="form-control" id="txtNrodocument" placeholder="Nro-document"
-                          maxlength="8" required autocomplete="off">
-                        <label for="txtNrodocument">Nro de documento</label>
-                      </div>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="txtNum-card" placeholder="No-card" required
-                        autocomplete="off">
-                      <label for="txtNum-card">Número de tarjeta</label>
-                    </div>
-
-                    <div>
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" value="" id="chkCardName">
-                        <label class="form-check-label" for="chkCardName">
-                          Recordar datos
-                        </label>
-                      </div>
-                      <div class="form-floating mb-3 d-none" id="inputToShow">
-                        <input type="text" class="form-control" id="txtNum-card" placeholder="No-card"
-                          autocomplete="off">
-                        <label for="txtNum-card">Nombre de tarjeta</label>
-                      </div>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                      <input type="password" class="form-control" id="floatingInput" placeholder="Password"
-                        maxlength="6" required autocomplete="off">
-                      <label for="floatingInput">Clave de internet de 6 dígitos</label>
-                      <div class="row">
-                        <div class="col-6">
-                          <a class="small" href="#">No tengo clave</a>
-                        </div>
-                        <div class="col-6 text-end">
-                          <a class="small" href="#">Olvide mi clave</a>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingPassword" placeholder="Code" required
-                        autocomplete="off">
-                      <label for="floatingPassword">Código</label>
-                      <div class="text-start">
-                        <a class="small" href="#">Cambiar código</a>
-                      </div>
-                    </div>
-
-                    <div class="d-grid">
-                      <button class="btn btn-lg btn-success btn-login text-uppercase fw-bold mb-2" type="submit"
-                        onclick="setDNI()">Continuar</button>
-                    </div>
-
-                  </form>
+                  </div>
                 </div>
               </div>
             </div>
